e636592570b2390022c3104ce5f0d591352b5883
kokkaiG9 <gowsky.9.2548@gmail.com>
2025-02-21 21:26:11 +0700

    finish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow){
+    WNDCLASSW wc = {0};
+
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL,IDC_ARROW);
+}
+
+int main(int argc,char* argv[]){
+    double sum = 0 ;
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }
+    
+    if(argc != 1){
+        for(int i = 1 ; i < argc ; i++){
+            sum+=atof(argv[i]);
+        }
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << endl;
+        cout << "---------------------------------";
+    }
+    
+    
+}
 
--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,54 @@
 #include <windows.h>
+#include <stdio.h>
 
+HWND textfield, button1, button2, button3, button4, textbox1, textbox2;
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
-		
+		case WM_CREATE:
+		textfield = CreateWindow("STATIC","Please input two numbers",WS_VISIBLE | WS_CHILD ,20,20,200,25,hwnd,NULL,NULL,NULL);
+		button1 = CreateWindow("BUTTON","+",WS_VISIBLE | WS_CHILD | WS_BORDER,20,120,25,25,hwnd,(HMENU) 1,NULL,NULL);
+		button2 = CreateWindow("BUTTON","-",WS_VISIBLE | WS_CHILD | WS_BORDER,70,120,25,25,hwnd,(HMENU) 2,NULL,NULL);
+		button3 = CreateWindow("BUTTON","*",WS_VISIBLE | WS_CHILD | WS_BORDER,120,120,25,25,hwnd,(HMENU) 3,NULL,NULL);
+		button4 = CreateWindow("BUTTON","/",WS_VISIBLE | WS_CHILD | WS_BORDER,170,120,25,25,hwnd,(HMENU) 4,NULL,NULL);
+		textbox1 = CreateWindow("EDIT","",WS_VISIBLE | WS_CHILD | WS_BORDER,20,60,200,20,hwnd,NULL,NULL,NULL);
+		textbox2 = CreateWindow("EDIT","",WS_VISIBLE | WS_CHILD | WS_BORDER,20,90,200,20,hwnd,NULL,NULL,NULL);
+		break;
+		case WM_COMMAND:
+		if (LOWORD(wParam) >= 1 && LOWORD(wParam) <= 4) 
+		{
+			char textsaved1[100], textsaved2[100];
+            GetWindowText(textbox1, textsaved1, 100);
+            GetWindowText(textbox2, textsaved2, 100);
+            double num1 = atof(textsaved1);
+            double num2 = atof(textsaved2);
+            double result = 0.0;
+
+			switch(LOWORD(wParam))
+		{
+			case 1:
+			result = num1 + num2;
+			break;
+			case 2:
+			result = num1 - num2;
+			break;
+			case 3:
+			result = num1 * num2;
+			break;
+			case 4:
+			if (num2 == 0) {
+				MessageBox(hwnd, "Division by zero!", "Error", MB_OK | MB_ICONERROR);
+				return 0;
+			}
+			result = num1 / num2;
+			break;
+		}
+			char resulttext[100];
+			sprintf(resulttext, " %f", result);
+			MessageBox(hwnd, resulttext, "Result", MB_OK);
+
+		}
+		break;
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -31,7 +76,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = CreateSolidBrush(RGB(250, 300, 0));
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,12 +86,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_OVERLAPPED | WS_VISIBLE ,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
+		// WS_VISIBLE|WS_OVERLAPPEDWINDOW
 
 	if(hwnd == NULL) {
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
